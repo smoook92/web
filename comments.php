566ee82fa9d3f56745d90372b5893bd3fb1dfe88
Smoook92 <axess-88@mail.ru>
2022-10-02 11:05:45 +0300

    Commit

--- comments.php
+++ comments.php
@@ -1,18 +1,19 @@
 <h1>Оставить комментарий</h1>
 
-<form action="comments.php" method="post" name="form">
+<form action="comment.php" method="post" name="form">
   <input name="name" type="text" placeholder="Ваше имя" />
   <br>
   <textarea cols="32" name="message" rows="5" placeholder="Текст сообщения"></textarea>
   <br>
-  <input name="submit" type="submit" value="Отправить" />
+    <input type="hidden" name="single_id" value="150" />
+    <input name="submit" type="submit" value="Отправить" />
 </form>
 
 <h1>Коментарии к статье:</h1>
 <!-- <h3>К этой статье пока никто не оставил комментарий.</h3> -->
 <div class="comment">
     <p class="sulez">
-        <img src="/include/img/user.png" ; alt="Фото пользователя"><strong>Сорин</strong>
+        <img src="/include/img/user.png" alt="Фото пользователя"><strong>Сорин</strong>
     </p>
     <p class="commentText">Интересная статья</p>
     <p class="sulez-2">
